d6fb66d05f94b069a37e5cd41531e9bc7ad43ee4
vcbonani <65261733+vcbonani@users.noreply.github.com>
2020-06-22 22:23:05 -0300

    Atualizacao do codigo de movimento da personagem
    Atualizacao do codigo de movimento da personagem

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -2,36 +2,26 @@ class Personagem{
   
   constructor(imagem){
     this.imagem = imagem;
-    this.matriz = [
-      [0   , 0  ],
-      [220 , 0  ],
-      [440 , 0  ],
-      [660 , 0  ],
-      [0   , 270],
-      [220 , 270],
-      [440 , 270],
-      [660 , 270],
-      [0   , 540],
-      [220 , 540],
-      [440 , 540],
-      [660 , 540],
-      [0   , 810],
-      [220 , 810],
-      [440 , 810],
-      [660 , 810],
-    ];
     this.frameAtual = 0;
   }
   
   exibe(){
-    image(this.imagem,0,height-135,110,135,this.matriz[this.frameAtual][0],this.matriz[this.frameAtual][1],220,270);
+    this.alturaPersonagem = 135;
+    this.larguraPersonagem = 110;
+    this.tamX = 220;
+    this.tamY = 270;
+    let x = this.frameAtual % 4 * this.tamX;
+    let y = Math.floor(this.frameAtual / 4) * this.tamY;
+  
+    image(this.imagem, 0, height- this.alturaPersonagem, this.larguraPersonagem, this.alturaPersonagem, x, y, this.tamX, this.tamY); 
     this.anima();
+
   }
   
   anima(){
     this.frameAtual++;
     
-    if(this.frameAtual >= this.matriz.length - 1){
+    if(this.frameAtual > 15){
       this.frameAtual = 0; 
     }
     
